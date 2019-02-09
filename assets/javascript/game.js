61ac0435617e475c88af43dec6c92857ead8012f
Pickle Rick <dmryan711@gmail.com>
2019-02-09 17:57:31 -0500

    Version 1 | Need to check for audio bugs and remove console logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,11 +11,13 @@ function Character(cName, cHealth, cImagePath, cSelectedByPlayer, cdefeatedByPla
   }
 
 var undefeatedCount = 8;
-
 var characterBank = [];
 var indexOfCurrentPlayer;
-var indexOfCurrentOpponent;
-var health = 100;
+var indexOfCurrentOpponent = 0;
+var health = 5;
+var playerHealth = 15;
+var counterAttack = 1;
+var playerAttack = 5;
 
 
 //CLICK EVENT
@@ -25,33 +27,94 @@ $("#click-to-play").click(function(){
 
 //ATTACK CLICK
 $("#attack-button").click(function(){
-    $("#attack-message").text(characterBank[indexOfCurrentPlayer].attackPower);
-    console.log(characterBank[indexOfCurrentPlayer].attackPower);
-    $("#attack-message").effect("puff", { }, 1000,function(){
-        $("#attack-message").text("");
-        //counter attack
-        attack(indexOfCurrentOpponent);
-    } );
-    
+    $(this).attr("disabled", "true");
     attack(indexOfCurrentPlayer);
 });
 
 function attack(playerOrOpponentIndex){
-
+    var randomAttackAmount;
     if(playerOrOpponentIndex === indexOfCurrentPlayer){
-        characterBank[indexOfCurrentOpponent].health -= characterBank[indexOfCurrentPlayer].attackPower;
-        console.log("Player Attack");
-        loadOpponent(characterBank[indexOfCurrentOpponent]);
-        $("#opponent").effect("shake");
+        randomAttackAmount = randomAttack(characterBank[indexOfCurrentPlayer],indexOfCurrentPlayer);
+        characterBank[indexOfCurrentOpponent].health -= randomAttackAmount;
+        $("#attack-message").text("You Attacked: "+randomAttackAmount);
+        $("#opponent").effect("shake",{},500);
+        $("#attack-message").effect("puff",{percent:110},1000,function(){
+            $("#attack-message").text("");
+            //Check if opponent is defeated
+            console.log("Check if opponent is still alive");
+            if(characterBank[indexOfCurrentOpponent].health > 0){
+                
+                loadOpponent(characterBank[indexOfCurrentOpponent]);
+                //counter attack
+                attack(indexOfCurrentOpponent);
+            }else{
+                    console.log("Opponent is dead");
+                    health +=5;
+                    characterBank[indexOfCurrentPlayer].health = playerHealth;
+                    loadPlayer(characterBank[indexOfCurrentPlayer]);
+                    defeatStateFor(indexOfCurrentOpponent);
+                    selectNextOpponent();
+            }
+            });
+        
+         
     }else if(playerOrOpponentIndex === indexOfCurrentOpponent){
-        characterBank[indexOfCurrentPlayer].health -= characterBank[indexOfCurrentOpponent].counterAttackPower;
-        console.log("Opponent Attack");
-        //Update Player UI here
-        loadPlayer(characterBank[indexOfCurrentPlayer]);
-        $("#player").effect("shake");
+        randomAttackAmount = randomAttack(characterBank[indexOfCurrentOpponent],indexOfCurrentOpponent);
+        characterBank[indexOfCurrentPlayer].health -= randomAttackAmount;
+        $("#attack-message").text("They Attacked: "+randomAttackAmount);
+        $("#player").effect("shake",{},500);
+        $("#attack-message").effect("puff", {percent:110}, 1000,function(){
+            $("#attack-message").text("");
+            //Check if player is defeated
+            console.log("Check if player is still alive");
+            if(characterBank[indexOfCurrentPlayer].health > 0){
+                
+               loadPlayer(characterBank[indexOfCurrentPlayer]);
+               
+                //counter attack
+               // attack(indexOfCurrentOpponent);
+            }else{
+                    console.log("Playter is dead");
+                    defeatStateFor(indexOfCurrentPlayer);
+                    //selectNextOpponent();
+            }
+            });
+        
+        $("#attack-button").removeAttr("disabled");
+        // console.log("Opponent Attack");
+        // //Update Player UI here
+        // loadPlayer(characterBank[indexOfCurrentPlayer]);
+        // $("#player").effect("shake",{},500,function(){
+        //     $("#attack-button").removeAttr("disabled");
+        // });
     }else{
         console.log("ERROR IN ATTACK")
     }
+
+    console.log("Player Health: " + characterBank[indexOfCurrentPlayer].health);
+    console.log("Opponent Health: " + characterBank[indexOfCurrentOpponent].health);
+
+}
+
+function randomAttack(characterObject, indexOfPlayerOrOpponent){
+
+    if(indexOfPlayerOrOpponent === indexOfCurrentPlayer){
+        var actualAttack = Math.floor(Math.random() * characterObject.attackPower) + 1;
+        console.log("Player Attack actual: " + actualAttack);
+        console.log("Player Base Attack: " + characterObject.attackPower);
+            return actualAttack
+          
+    }else if(indexOfPlayerOrOpponent === indexOfCurrentOpponent){
+        var actualAttack = Math.floor(Math.random() * characterObject.counterAttackPower) + 1;
+
+        console.log("Opponent Counter actual: " + actualAttack);
+        console.log("Opponent Base Counter: " + characterObject.counterAttackPower);
+            return actualAttack
+
+    }else{
+        console.log("Error in random attack");
+    }
+   
 }
 
 // if (typeof jQuery.ui != 'undefined') {
@@ -64,8 +127,9 @@ function attack(playerOrOpponentIndex){
 $(".player-selection-card").click(function (){
     console.log("Click player selection");
     console.log(this);
+    
+    
     toggleSelectionOrBattleContainer($(this));
-    //playSelectionAudio();
     setBattleContainer($(this));
     var themeSong = $("#starWarsThemeSong");
     themeSong.trigger('pause');
@@ -101,7 +165,6 @@ function toggleSelectionOrBattleContainer(element){
     }else{
         console.log("Error");
     }
-
 }
 
 // BATTLE CONTAINER
@@ -114,47 +177,114 @@ function setBattleContainer(selectedByPlayer){
             characterBank[i].isSelectedByPlayer = true;
             //ADD CHARACTER TO PLAYER UI
             indexOfCurrentPlayer = i;
-            characterBank[i].attackPower = 1;
+            characterBank[i].attackPower = playerAttack;
+            characterBank[i].health = playerHealth;
             loadPlayer(characterBank[i]);
             //break;
         }
     }
-    //FIND RANDOM OPPONENT
-    selectRandomOpponent();
+    //FIND SEQUENTIAL  OPPONENT
+    selectNextOpponent();
 }
 
 
-    function selectRandomOpponent(){
-    //Are all characters defeated?
-      if(undefeatedCount > 0){
-
-        
-        //If not pick a character
-        var randomIndex = Math.floor(Math.random()*characterBank.length);
-        var opponent = characterBank[randomIndex];
-
-        //If character is qualified
-        if(!opponent.isDefeatedByPlayer && !opponent.isSelectedByPlayer){
-            //Choose Character
-            indexOfCurrentOpponent = randomIndex;
-            loadOpponent(opponent);
-        //If character is not qualified, try again
+    function selectNextOpponent(){
+        if(undefeatedCount > 0){  
+            var nextOpponent = characterBank[indexOfCurrentOpponent];
+            if(!nextOpponent.isDefeatedByPlayer && !nextOpponent.isSelectedByPlayer){
+                console.log("Found Next Opponent: " + characterBank[indexOfCurrentOpponent]);
+                characterBank[indexOfCurrentOpponent].health = health;
+                loadOpponent(characterBank[indexOfCurrentOpponent]);
+            }else{
+                indexOfCurrentOpponent++;
+                selectNextOpponent();
+            }
+            
         }else{
-            selectRandomOpponent();
+            //Show Win Message
+            console.log("You Won!");
+            alert("You Won!");
+            restartGameWithModal(false);
         }
-      }else{
-        //All Characters are defeated, show win message
-      } 
     }
 
+// //CHECK FOR DEFEAT
+function defeatStateFor(indexOfPlayerOrOpponent){
+   //Update users to defeat
+   console.log(indexOfCurrentOpponent);
+
+
+   if(indexOfPlayerOrOpponent === indexOfCurrentOpponent){
+        characterBank[indexOfCurrentOpponent].isDefeatedByPlayer = true;
+        console.log("Opponent Defeat Status"+characterBank[indexOfCurrentOpponent].isDefeatedByPlayer);
+        // $("#opponent").effect("explode");
+        $("#opponent-health").text("0");
+        $("#attack-button").removeAttr("disabled");
+        undefeatedCount--;
+
+        //CHANGE PLAYER ATTACK
+        characterBank[indexOfCurrentPlayer].attackPower++;
+
+        console.log("Undefeated Count "+undefeatedCount);
+   }else if(indexOfPlayerOrOpponent === indexOfCurrentPlayer){
+       console.log("Player Dead")
+       alert("You Lost!");
+       restartGameWithModal(false);
+
+   }
+   
+
+}
+
+function restartGameWithModal(modalBool){
+    if(modalBool){
+
+    }else{
+        undefeatedCount = 8;
+        indexOfCurrentOpponent = 0;
+        indexOfCurrentPlayer = 0;
+        characterBank.length = 0;
+        counterAttack = 1;
+        health = 10;
+        $("#battle-container").removeClass('visible').addClass('hidden');
+        hideGameStart();
+        addCharacterstoHeroBank();
+        console.log(characterBank);
+    }
+}
+
+
+    // function selectRandomOpponent(){
+    // //Are all characters defeated?
+    //   if(undefeatedCount > 0){
+
+        
+    //     //If not pick a character
+    //     var randomIndex = Math.floor(Math.random()*characterBank.length);
+    //     var opponent = characterBank[randomIndex];
+
+    //     //If character is qualified
+    //     if(!opponent.isDefeatedByPlayer && !opponent.isSelectedByPlayer){
+    //         //Choose Character
+    //         indexOfCurrentOpponent = randomIndex;
+    //         loadOpponent(opponent);
+    //     //If character is not qualified, try again
+    //     }else{
+    //         selectRandomOpponent();
+    //     }
+    //   }else{
+    //     //All Characters are defeated, show win message
+    //   } 
+    // }
+
     function loadOpponent(opponent){
-        $("#opponent-health").text(opponent.health);
+        $("#opponent-health").text("Hit Points: "+opponent.health);
         $("#opponent-picture").attr('src',opponent.imagePath);
         $("#opponent-name").text(opponent.name);
     }
 
     function loadPlayer(player){
-        $("#player-health").text(player.health);
+        $("#player-health").text("Hit Points: " +player.health);
         $("#player-picture").attr('src',player.imagePath);
         $("#player-name").text(player.name);
     }
@@ -175,16 +305,14 @@ $(document).ready(function(){
     }
 
         function iterateThroughCharacterRow(characterRowElement){
-            var counterAttack = 1;
+            
             characterRowElement.children().each(function( index ) {
                 var imagePath = $(this).children('img').attr('src');
                 var name = $(this).children('div').children('h5').text();
                 var elementId = getElementIdFromSelectionCard(this);
-                var health = 100;
                 var attack = 0;
-                counterAttack++;
                 characterBank.push(new Character(name,health,imagePath,false,false,elementId,attack,counterAttack));
-                
+                counterAttack++;
               }
         
               );
